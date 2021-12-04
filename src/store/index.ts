493b6c995ddb233c992580184b6c794cca377f5c
lyq <953342912@qq.com>
2021-12-04 12:30:34 +0800

    feat: 添加pinia为状态管理

---
+++ src/store/index.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+// 第一种
+// export const useCounterStore = defineStore('counter', {
+//   state: () => {
+//     return { count: 0 }
+//   },
+//   // could also be defined as
+//   // state: () => ({ count: 0 })
+//   actions: {
+//     increment() {
+//       this.count++
+//     },
+//   },
+// })
+
+// 第二种
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+  function increment() {
+    count.value++
+  }
+
+  return { count, increment }
+})
