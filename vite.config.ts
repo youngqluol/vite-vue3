a1d4f3c4503016f25d63f72a89cb69f71c05a0a1
yongqing_luo <yongqing_luo@kingdee.com>
2021-12-20 10:32:43 +0800

    配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+function resolvePath(p: string) {
+  return path.resolve(process.cwd(), p);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      // TODO: 未生效
-      '@': path.resolve(__dirname, '/src')
+      '@': resolvePath('src')
+    }
+  },
+  server: {
+    fs: {
+      strict: false
     }
   }
-})
+});
